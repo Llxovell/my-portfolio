c352bca2d9b1daa184e43c894950ebef76ce6f08
Llxovell <005961913@coyote.csusb.edu>
2022-02-11 21:48:26 -0800

    started contact page

---
+++ portfolio/components/ContactContent.js
@@ -0,0 +1,41 @@
+import contact_style from "../styles/Contact.module.css";
+import CopySVG from "./CopySVG";
+
+export default function ProjectsContent() {
+	function ContactInput(props) {
+		return (
+			<>
+				<h2 className={contact_style.input_caption}>{props.caption}</h2>
+				<input
+					type="text"
+					className={`default-text-container ${contact_style.contact_input}`}
+					placeholder={props.placeholder}
+				/>
+			</>
+		);
+	}
+
+	return (
+		<div className={`default-container ${contact_style.contact_container}`}>
+			<h1 className={contact_style.title}>
+				Get in <span>touch</span>
+			</h1>
+			<div className={`default-text-container ${contact_style.email_link}`}>
+				<p>levellmack@gmail.com</p>
+				<CopySVG />
+			</div>
+			<form
+				className={contact_style.input_form}
+				action="mailto:levellmack@gmail.com"
+				method="post"
+				encType="multipart/form-data"
+			>
+				<ContactInput caption="Name" placeholder="Full name" />
+				<ContactInput caption="Email" placeholder="Email address" />
+				<ContactInput caption="Subject" placeholder="Email subject" />
+				<ContactInput caption="Message" placeholder="Your message here" />
+				<input type="submit" className={contact_style.submit} value="SEND" />
+			</form>
+		</div>
+	);
+}
--- portfolio/pages/contact.js
+++ portfolio/pages/contact.js
@@ -1,9 +1,15 @@
 import PageTemplate from "../components/PageTemplate";
+import ContactContent from "../components/ContactContent";
 
 export default function Contact() {
 	return (
 		<>
-			<PageTemplate fourth="#D55672" text="</Contact>" selected="contact" />
+			<PageTemplate
+				fourth="#D55672"
+				text="</Contact>"
+				selected="contact"
+				content={<ContactContent />}
+			/>
 		</>
 	);
 }
